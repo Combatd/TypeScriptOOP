7b4761ab28cab4e6d6a0fd1eb7940be65b4a81c3
Combatd <combatdstar@gmail.com>
2021-03-22 11:39:42 -0700

    Declare protected age, Define Cat#birthday

--- Ch03/16-protected.ts
+++ Ch03/16-protected.ts
@@ -1,5 +1,5 @@
 export class Animal {
-    age: number;
+    protected age: number;
     legs: number;
     name: string;
 
@@ -9,15 +9,22 @@ export class Animal {
         this.name = name;
     }
 }
-
+// child
 export class Cat extends Animal {
     constructor(data: { age: number, legs: number, name: string }) {
         super(data.age, data.legs, data.name);
     }
-}
 
+    get birthday(): number {
+        return this.age + 1;
+    }
+}
+// shape
 export class Dog implements Animal {
     age: number;
     legs: number;
     name: string;
 }
+
+const cat = new Cat( { age: 19, legs: 4, name: 'Baby' });
+cat.age; // not accessible, only accessible in Base and Derived Classes
