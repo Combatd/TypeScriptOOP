825900f3746e1ea9a9c7cb92ac85a4de546635fe
Combatd <combatdstar@gmail.com>
2021-03-22 09:10:19 -0700

    Create User class

--- Ch01/challenge-classes-1.ts
+++ Ch01/challenge-classes-1.ts
@@ -7,3 +7,26 @@
  * 3. Add a get to return fullName.
  * 4. Add a method to check if your email param matches the user's current email.
  */
+
+class User {
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    set setEmail(email: string) {
+        this.email = email;
+    }
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    checkEmailMatches(email: string) {
+        return this.email === email;
+    }
+}
