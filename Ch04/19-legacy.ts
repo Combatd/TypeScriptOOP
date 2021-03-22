5a6744cf628895276f77b3e6257fde6167a7a472
Combatd <combatdstar@gmail.com>
2021-03-22 12:16:16 -0700

    Declare UserLegacy prototype

--- Ch04/19-legacy.ts
+++ Ch04/19-legacy.ts
@@ -9,3 +9,16 @@ export class User {
         return this.email === email;
     }
 }
+
+// prototype class using TypeScript
+function UserLegacy(firstName: string, lastName: string, email: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+
+    this.doesEmailMatch = function(email): boolean {
+        return this.email === email;
+    }
+}
+
+const user = new UserLegacy('Dylan', 'Israel', 'DylansEmail909@gmail.com');
