26e09dcbff187040a572030f0a78e8f65fa37116
Combatd <combatdstar@gmail.com>
2021-03-22 12:01:36 -0700

    Define class Message constructor and readonly id attribute

--- Ch03/18-readonly.ts
+++ Ch03/18-readonly.ts
@@ -1,5 +1,14 @@
 export class Message {
+    readonly id: string;
     title: string;
     message: string;
     isSent: boolean;
+
+    constructor(id: string) {
+        this.id = id;
+    }
 }
+
+const message = new Message('abc123');
+message.id; // returns the value
+message.id = '123abc'; // error!
