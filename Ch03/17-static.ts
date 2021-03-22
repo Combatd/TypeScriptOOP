7e190d5193174ae571acbf2d4f3e9c78be704d6a
Combatd <combatdstar@gmail.com>
2021-03-22 11:58:50 -0700

    Define prototypal Messages::getValidMessages

--- Ch03/17-static.ts
+++ Ch03/17-static.ts
@@ -4,6 +4,19 @@ export class Message {
     isSent: boolean;
 }
 
-export class Messages {
+export class Messages extends Array<Message> {
+    public getValidMessages(messages: Messages[]: Message[]) {
+        return this.filter( (value) => {
+            return value.message.trim().length > 0;
+        });
+    }
 
+    // static methods are always public, under the hood this is Messages.prototype.getValidMessages
+    public static getValidMessages(messages: Message[]): Message[] {
+        return messages.filter( (value) => {
+            return value.message.trim().length > 0;
+        });
+    }
 }
+
+Messages.getValidMessages([]);
