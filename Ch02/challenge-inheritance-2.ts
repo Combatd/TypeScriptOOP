191923939eee393a0b81cb25b837f32c0f62222f
Combatd <combatdstar@gmail.com>
2021-03-22 10:24:25 -0700

    Set properties of class Admin

--- Ch02/challenge-inheritance-2.ts
+++ Ch02/challenge-inheritance-2.ts
@@ -25,6 +25,9 @@ export class User {
 export class Admin extends User {
     constructor(firstName: string, lastName: string, email: string) {
         super();
+        this.firstName = firstName;
+        this.lastName = lastName
+        this.email = email;
     }
 }
 
