5e24c58a5eee21e02fc20e0ebd71a51b01e4b35b
Combatd <combatdstar@gmail.com>
2021-03-22 09:49:58 -0700

    OOverride derived attributes in Cat class

--- Ch02/11-super-2.ts
+++ Ch02/11-super-2.ts
@@ -2,21 +2,30 @@ export class Animal {
     age: number;
     legs: number;
     name: string;
-    
+    lives: number = 1;
+
     constructor(age: number, legs: number, name: string) {
         this.age = age;
         this.legs = legs;
         this.name = name;
     }
 
+    meow(): string {
+        return 'ROAR! ROAR! ROAR!';
+    }
 }
 
 export class Cat extends Animal {
+    lives: number = 9;
+
     constructor(data: { age: number, legs: number, name: string }) {
         super(data.age, data.legs, data.name);
     }
 
     meow(): string {
+        super.meow();
+        super.lives; // returns 9, overrides property
+
         return 'MEOW! HISS! HISS!';
     }
 }
