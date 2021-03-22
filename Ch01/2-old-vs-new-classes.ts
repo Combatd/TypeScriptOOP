febc0b8408ad98fc061c6e59f8c51cb2414a5b09
Combatd <combatdstar@gmail.com>
2021-03-22 08:05:54 -0700

    Declare prototype Message2

--- Ch01/2-old-vs-new-classes.ts
+++ Ch01/2-old-vs-new-classes.ts
@@ -1,6 +1,15 @@
+// class
 export class Message {
     title;
     message;
     id;
 }
 
+// prototype
+function Message2(title, message, id) {
+    this.title = title;
+    this.message = message;
+    this.id = id;
+}
+
+const message2 = new Message2('Hello', 'Wpr;d', 'Infinity');
