9ebd48c40913d6f1d315b917b4fa098e6dc96385
Combatd <combatdstar@gmail.com>
2021-03-22 11:34:05 -0700

    Define Animal::ourLegs

--- Ch03/15-public-vs-private.ts
+++ Ch03/15-public-vs-private.ts
@@ -8,10 +8,19 @@ export class Animal {
         this.legs = legs;
         this.name = name;
     }
+
+    public get ourLegs() {
+        return this.ourLegs;
+    }
 }
 
 export class Cat extends Animal {
     constructor(data: { age: number, legs: number, name: string }) {
         super(data.age, data.legs, data.name);
     }
+
 }
+
+const cat = new Cat( { age: 19, legs: 4, name: 'Baby' });
+cat.age;
+cat.legs; // not accessible if private, would need to use getter
